0ca5429e353a32a0ca478e6035a97d5cec6a7d9c
Stuti08coder <76516771+Stuti08coder@users.noreply.github.com>
2021-07-14 09:27:50 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -2,26 +2,46 @@ const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
 const Constraint = Matter.Constraint;
+var bgImage
+var particles = [];
+var snows = [];
+var snowfall =[];
+var snowfallsize=300;
+
+
 
 
 function setup() {
   createCanvas(800,400);
   engine = Engine.create();
-
+bgImage = loadImage("snow1.jpg")
   
   world = engine.world;
 
-  snow1 = new Snow(400,200,20)
+  for (var j = 75; j <=width; j=j+50) { 
+    particles .push(new Particles(j,75));
+  }
+
+
+
+  snow1 = new Snow(400,200,15)
+
 }
 
 function draw() {
-  background("yellow");  
+  background(bgImage);  
   Engine.update(engine);
  
   
+  if(frameCount%50===0){
+    snows.push(new Snows(random(width/2-10, width/2+10),15,15));
+    
+    
+    }
+  
   
 
-
+fill("white")
 snow1.display();
 
 
